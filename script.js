cf3b34744256df82e9ed1ff735a0230e346ed205
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-10-25 16:46:23 +0200

    feat: add comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Constante pour créer une fenêtre de navigateur
 const { app, BrowserWindow } = require('electron/main')
+// Constante pour gérer les chemins de fichiers
 const path = require('node:path')
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    // Script de préchargement de la fenêtre
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -12,18 +15,20 @@ function createWindow () {
 
   win.loadFile('index.html')
 }
-
+//Quand l'app est prête, on appelle la fonction createWindow
 app.whenReady().then(() => {
   createWindow()
 
+  // Si l'application est active sans fenêtre, on en crée une nouvelle
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
   })
 })
-
+// Détecteur d'événement pour fermer l'application
 app.on('window-all-closed', () => {
+    // Darwin est le noyau de MacOS, donc si on est pas sur MacOS, on ferme l'application
   if (process.platform !== 'darwin') {
     app.quit()
   }
