a8396b32ef544c320e3fee9a8915697564df5c83
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-11-02 02:01:58 +0100

    feat: kept only electronjs basecode

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
-// Code pour le tirage au sort (en commentaire car doit d'abord coder le HTML/CSS)
-
-/* const names = [];
-
-function addName() {
-    const nameInput = document.getElementById('name');
-    const name = nameInput.value.trim();
-    if (name) {
-        names.push(name);
-        const li = document.createElement('li');
-        li.textContent = name;
-        document.getElementById('names').appendChild(li);
-        nameInput.value = '';
-    }
-}
-
-function reset() {
-    names.length = 0;
-    document.getElementById('names').innerHTML = '';
-}
-
-function randomize() {
-    if (names.length > 0) {
-        const randomIndex = Math.floor(Math.random() * names.length);
-        alert(`Nom sélectionné : ${names[randomIndex]}`);
-    } else {
-        alert('Veuillez ajouter au moins un prénom.');
-    }
-}
-*/
-
 // Code générique pour Electron.js
-const { app, BrowserWindow } = require('electron/main');
-const path = require('node:path');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 920,
+        height: 800,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
